d9f7660d97bc392081de56620517ac9eae70eedc
Casey Runnells <crunnells@gmail.com>
2016-02-03 16:54:19 -0800

    updated html and initial js code

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-$(function(){
+var correctAnswers = ["13","22","31","42","51"]
+var userAnswers = []
 
-});
+document.getElementById("submit").onclick = function scoreTest()
+    {
+     
+
+    }  
